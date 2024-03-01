40a0853add1e790962e86b519451939f919bb3e6
majerzz <juliamjrzzz@gmail.com>
2024-03-01 21:07:11 +0600

    Using fetch

--- src/app/model/store/index.ts
+++ src/app/model/store/index.ts
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { productsApi } from 'app/api/products'
 import shoppingCartSlice from 'app/model/store/shoppingCartSlice'
 
 const store = configureStore({
-
   reducer: {
-    shoppingCart: shoppingCartSlice
-  }
-
+    shoppingCart: shoppingCartSlice,
+    [productsApi.reducerPath]: productsApi.reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productsApi.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>
