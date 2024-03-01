6d8632d8d3f2cabe5c83f0c417ee58fa83df3c35
majerzz <juliamjrzzz@gmail.com>
2024-03-01 16:20:35 +0600

    deleted unnecessery comments

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,7 +3,7 @@ import { GameCards } from 'widgets/GameCards/GameCards'
 
 const TestPage: React.FC = () => {
   return (
-    <GameCards idList={[1, 2, 3, 4, 5]} /> // временно, сюда пихнуть хуйню с новинками
+    <GameCards idList={[1, 2, 3, 4, 5]} />
   )
 }
 
